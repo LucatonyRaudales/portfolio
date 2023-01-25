d3ec7e96dc0ed040a3e4aec8fddcb864d977c33c
Tony Raudales <tony.raudales@scrummers.co>
2023-01-24 19:54:09 -0600

    adding devops engineer section

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    protocol: "https",
-    hostname: "wallpaper.dog",
-    port: "",
-    pathname: "dog/large/*",
-  },
   reactStrictMode: true,
 };
 
