979556aec050f003207fcaa204ac67b4f4297071
Lucatony Raudales <tony.raudales@scrummers.co>
2023-07-18 11:11:10 -0600

    adding social network icons

--- pages/components/Introduction.tsx
+++ pages/components/Introduction.tsx
@@ -2,17 +2,21 @@ import Typewriter from "typewriter-effect";
 import { FaHandSpock } from "react-icons/fa";
 import { BsMouse } from "react-icons/Bs";
 import Image from "next/image";
+import { GrLinkedin, GrTwitter } from "react-icons/Gr";
+import { useRouter } from "next/router";
 
 const First = () => {
+  const router = useRouter();
+
   return (
     <div className="snap-start h-screen w-screen justify-center items-center flex flex-col">
-        <Image
-          src="/me.jpeg"
-          className="rounded-full mb-10"
-          alt="Picture of the author"
-          width={300}
-          height={300}
-        />
+      <Image
+        src="/me.jpeg"
+        className="rounded-full mb-10"
+        alt="Picture of the author"
+        width={300}
+        height={300}
+      />
       <h1 className="text-lg font-bold text-white flex items-center">
         Hi, {"I'm "}
         <p className="text-blue-500 font-bold pl-2 text-2xl">
@@ -37,9 +41,24 @@ const First = () => {
           }}
         />
       </div>
+      <div className="flex mt-5">
+        <button
+          onClick={() =>
+            router.push("https://www.linkedin.com/in/tony-raudales")
+          }
+        >
+          <GrLinkedin color="white" size={30} />
+        </button>
+        <div className="p-4" />
+        <button onClick={() => router.push("https://twitter.com/LucatonnyR")}>
+          <GrTwitter color="white" size={30} />
+        </button>
+      </div>
       <div className="animate-bounce absolute bottom-5">
         <BsMouse color="white" size={30} className="animate-pulse mx-auto" />
-        <h2 className="text-white font-semibold text-sm animate-pulse mt-1">Scroll Down</h2>
+        <h2 className="text-white font-semibold text-sm animate-pulse mt-1">
+          Scroll Down
+        </h2>
       </div>
     </div>
   );
