8e8b41ab3c78731befbe924a355af08b358239dc
Lucatony Raudales <tony.raudales@scrummers.co>
2023-07-18 15:31:51 -0600

    Adding some stuff

--- pages/components/Frontend.tsx
+++ pages/components/Frontend.tsx
@@ -1,36 +1,31 @@
-import Image, { StaticImageData } from "next/image";
-import devopsPicture from "@/public/Devops.png";
-import nextjsImage from "@/public/nextjs.png";
-import flutterImage from "@/public/flutter.png";
-import reactjsImage from "@/public/reactjs.png";
-import tailwindcssImage from "@/public/tailwindcss.png";
+import Image from "next/image";
 
 const FrontendSection = () => {
   const technologies: TechDescriptionInterface[] = [
     {
       title: "NextJS",
-      image: nextjsImage,
+      image: "/nextjs.png",
       subTitle: "Framework Web",
       description:
         "Expert in Kubernetes with experience in implementing and managing containerized applications using this open-source orchestration system. Skilled in creating microservices architectures, automatic scaling and managing containers for high availability.",
     },
     {
       title: "Flutter",
-      image: flutterImage,
+      image: "/flutter.png",
       subTitle: "Mobile",
       description:
         "Experienced in ArgoCD, a declarative continuous delivery tool for Kubernetes. Skilled in deploying and managing applications using GitOps methodologies and configuring ArgoCD for multi-cluster and multi-namespace deployments. Strong understanding of Kubernetes resource manifests and ArgoCD's role in automating the deployment pipeline.",
     },
     {
       title: "Reactjs",
-      image: reactjsImage,
+      image: "/reactjs.png",
       subTitle: "Library Web",
       description:
         "Expert in Terraform, a leading Infrastructure as Code (IaC) tool. Skilled in provisioning and managing infrastructure across multiple cloud platforms using Terraform's HashiCorp Configuration Language (HCL). Experience in creating reusable modules and automating infrastructure deployments with Terraform.",
     },
     {
       title: "Tailwind CSS",
-      image: tailwindcssImage,
+      image: "/tailwindcss.png",
       subTitle: "CSS Framework",
       description:
         "Expert in Jenkins with experience in automating continuous integration and continuous delivery processes. Skilled in configuring and using various plugins for integrating with tools and systems. Experience in managing and monitoring CI/CD processes using Jenkins' web interface.",
@@ -41,7 +36,7 @@ const FrontendSection = () => {
       <div className="w-1/2 h-screen flex flex-col items-center justify-center text-center ">
         <Image
           className="ml-12"
-          src={devopsPicture}
+          src={"/Devops.png"}
           alt="Picture of the author"
           width={600}
           height={600}
@@ -88,7 +83,7 @@ export const TechDescriptionComponent = ({
 
 export interface TechDescriptionInterface {
   title: string;
-  image: StaticImageData;
+  image: string;
   subTitle: string;
   description: string;
 }
