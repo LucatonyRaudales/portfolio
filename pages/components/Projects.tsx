88070fa297db44bf8bafe08a05720daa163b031e
Lucatony Raudales <tony.raudales@scrummers.co>
2023-07-18 10:07:39 -0600

    adding some stuff

---
+++ pages/components/Projects.tsx
@@ -0,0 +1,7 @@
+const ProjectsSection = () => {
+    return (
+        <div className="snap-start h-screen w-full flex flex-col items-center justify-center text-center text-white">this is my projects</div>
+    )
+}
+
+export default ProjectsSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import Initialize from "./components/Introduction";
 import DevOpsSection from "./components/DevOps";
 import BackendSection from "./components/Backend";
 import FrontendSection from "./components/Frontend";
+import ProjectsSection from "./components/Projects";
 
 export default function Home() {
   return (
     <div className="snap-y snap-mandatory bg-gradient-to-br from-black to-gray-900 h-screen w-screen overflow-auto overflow-x-hidden scrollbar-hide">
       <First />
+      <ProjectsSection />
       <DevOpsSection />
       <BackendSection />
       <FrontendSection />
